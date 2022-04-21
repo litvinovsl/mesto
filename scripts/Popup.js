5d11ea52e9c1ae46b35621dfccd37e4df6b6c52b
Sergey Litvinov <litvinows9@gmail.com>
2022-04-21 21:49:49 +0300

    add Popup.js file with Popup class

---
+++ scripts/Popup.js
@@ -0,0 +1,33 @@
+export default class Popup {
+    constructor(popupSelector){
+        this._popupSelector = popupSelector;
+        this._popupCloseButton = document.querySelector('.popup__button-close');
+        this._popupOverlay = document.querySelector('.popup__overlay');
+        this._handleEscClose = this._handleEscClose.bind(this);
+    }
+
+    open(){
+        this._popupSelector.classList.add('popup_opened');
+        document.addEventListener('keyup', this._handleEscClose);
+    }
+
+    close(){
+        this._popupSelector.classList.remove('popup_opened');
+        document.removeEventListener("keyup", this._handleEscClose);
+    }
+
+    _handleEscClose(e) {
+        if (evt.key === "Escape") {
+            this.close();
+        }
+    }
+
+    setEventListeners(){
+        this._popupOverlay.addEventListener('click', () => {
+            this.close;
+        });
+        this._popupCloseButton.addEventListener('click', () => {
+            this.close;
+        });
+    }
+}
