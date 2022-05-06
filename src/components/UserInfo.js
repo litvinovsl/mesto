6bac293f41d5b57b7e5958b9a1e06aa56166245d
Sergey Litvinov <litvinows9@gmail.com>
2022-05-06 16:57:55 +0300

    add update avatar function

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,7 +1,8 @@
 export default class UserInfo {
-    constructor({nameSelector, aboutSelector}){
+    constructor({nameSelector, aboutSelector, avatarSelector}){
         this._nameElement = document.querySelector(nameSelector);
         this._aboutElement = document.querySelector(aboutSelector);
+        this._avatarElement = document.querySelector(avatarSelector);
     }
 
     getUserInfo(){
@@ -9,11 +10,14 @@ export default class UserInfo {
             name: this._nameElement.textContent,
             about: this._aboutElement.textContent
         }
-        
     }
 
     setUserInfo({name, about}){
         this._nameElement.textContent = name;
         this._aboutElement.textContent = about;
     }
+
+    setUserAvatar({ avatarLink }) {
+        this._avatarElement.src = avatarLink;
+      }
 }
--- src/page/index.js
+++ src/page/index.js
@@ -5,6 +5,7 @@ import Section from '../components/Section.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
+import Api from '../components/Api.js';
 import {
   editButt,
   editUserForm,
@@ -18,6 +19,14 @@ import {
   profileAvatar
 } from '../utils/constants.js';
 
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-40',
+  headers: {
+    authorization: '6cbd57a7-9435-4249-951e-f8947dba9801',
+    'Content-Type': 'application/json'
+  }
+}); 
+
 function createCard(item){
   const card = new Card(item, '#card-template', (data) => {
     imagePopup.open(data.name, data.link);
@@ -26,7 +35,15 @@ function createCard(item){
   return cardElement
 }
 
-const userInfo = new UserInfo({nameSelector: '.profile__username', aboutSelector: '.profile__about-user'});
+//==============================================================================================================
+//все с профилем
+
+const userInfo = new UserInfo({
+  nameSelector: '.profile__username', 
+  aboutSelector: '.profile__about-user', 
+  avatarSelector: '.profile__avatar'
+});
+
 const popupUser = new PopupWithForm('#popup-user', (user) => {
   userInfo.setUserInfo(user);
 });
@@ -39,6 +56,40 @@ editButt.addEventListener('click', function () {
   popupUser.open();
 });
 
+
+
+//==========================================================================
+//все для аватара
+
+const popupAvatar = new PopupWithForm('#popup-update-avatar', (user) => {
+api.updateProfileAvatar({avatar: user.link}).then((data) => {
+  userInfo.setUserAvatar({avatarLink: data.avatar});
+});
+
+
+});
+
+popupAvatar.setEventListeners();
+
+avatarBatton.addEventListener('click', function(){
+  avatarFormValidation.resetValidation();
+  popupAvatar.open();
+})
+
+//==========================================================================
+//Валидация
+
+const editPopupValidator = new FormValidator(validationSettings, editUserForm);
+const createPopupValidator = new FormValidator(validationSettings, popupCreate);
+const avatarFormValidation = new FormValidator(validationSettings, updateAvatarForm);
+avatarFormValidation.enableValidation();
+editPopupValidator.enableValidation();
+createPopupValidator.enableValidation();
+
+//===========================================================================
+//================================================================================================================
+//все с карточками
+
 const popupCreateCard = new PopupWithForm('#popup-create', (item) => {
   const cardElement = createCard(item);
   cardSection.addItem(cardElement);
@@ -50,45 +101,92 @@ plusButt.addEventListener('click', function(){
   popupCreateCard.open();
 });
 
-//==========================================================================
-
-const popupAvatar = new PopupWithForm('#popup-update-avatar', (user) => {
-  profileAvatar.src = user.link;
-});
-const avatarFormValidation = new FormValidator(validationSettings, updateAvatarForm);
-avatarFormValidation.enableValidation();
-popupAvatar.setEventListeners();
-
-avatarBatton.addEventListener('click', function(){
-  avatarFormValidation.resetValidation();
-  popupAvatar.open();
-})
-
-//==========================================================================
-
-const editPopupValidator = new FormValidator(validationSettings, editUserForm);
-const createPopupValidator = new FormValidator(validationSettings, popupCreate);
-editPopupValidator.enableValidation();
-createPopupValidator.enableValidation();
-
+//==================================================
 
 const imagePopup = new PopupWithImage('#popup-card');
 imagePopup.setEventListeners();
 
 const cardSection = new Section({
   renderer: function(item) {
-    // const card = new Card(item, cardSel, '#card-template', () => {
-    //   imagePopup.open(item.name, item.link);
-    // });
-    // const cardElement = card.generateCard();
-    // return cardElement
-    // console.log(this);
-    // debugger
-    
     const cardElement = createCard(item);
     this.addItem(cardElement);
   },  
 }, '.elements');
 cardSection.renderItems(initialCards);
 
+//======================================================================
 
+// профиль пользователя(имя о себе аватар _id и тд)
+// fetch('https://nomoreparties.co/v1/cohort-40/users/me', {
+//   headers: {
+//     authorization: '6cbd57a7-9435-4249-951e-f8947dba9801'
+//   }
+// })
+//   .then(res => res.json())
+//   .then((data) => {
+//     console.log(data);
+// });
+
+
+//массив карточек с инфой
+// fetch('https://mesto.nomoreparties.co/v1/cohort-40/cards', {
+//   headers: {
+//     authorization: '6cbd57a7-9435-4249-951e-f8947dba9801'
+//   }
+// })
+//   .then(res => res.json())
+//   .then((data) => {
+//     // console.log(data);
+// });
+
+//avatar
+
+// fetch('https://mesto.nomoreparties.co/v1/cohort-40/users/me/avatar', {
+//   method: 'PATCH',
+//   headers: {
+//     authorization: '6cbd57a7-9435-4249-951e-f8947dba9801'
+//   },
+//   headers: {
+//     authorization: '6cbd57a7-9435-4249-951e-f8947dba9801',
+//    'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     avatar: 'https://www.50languages.com/template/img/58755931.jpg',
+//   })
+// })
+  
+
+//запрос на редактировнные данные профиля
+// fetch('https://mesto.nomoreparties.co/v1/cohort-40/users/me', {
+//   method: 'PATCH',
+//   headers: {
+//     authorization: '6cbd57a7-9435-4249-951e-f8947dba9801',
+//     'Content-Type': 'application/json'
+//   },
+  // body: JSON.stringify({
+  //   name: 'Litvinov Sergey',
+  //   about: 'student'
+  // })
+// })
+//   .then(res => res.json())
+//   .then((data) => {
+//     // console.log(data);
+// });
+
+//добавление новой карточки
+// fetch('https://mesto.nomoreparties.co/v1/cohort-40/cards', {
+//   method: 'PATCH',
+//   headers: {
+//     authorization: '6cbd57a7-9435-4249-951e-f8947dba9801',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     name: 'Litvinov Sergey',
+//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+//   })
+// })
+//   .then(res => res.json())
+//   .then((data) => {
+//     console.log(data);
+// });
+  
